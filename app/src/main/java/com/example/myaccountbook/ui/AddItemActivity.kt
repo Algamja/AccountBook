b27bbfd35b1b546cc66a28d3f89cd24cec38a34e
PROJECT <rlawkaud11@naver.com>
2020-09-27 08:54:58 +0900

    [modified] Add Activity > make delete button

--- app/src/main/java/com/example/myaccountbook/ui/AddItemActivity.kt
+++ app/src/main/java/com/example/myaccountbook/ui/AddItemActivity.kt
@@ -2,6 +2,7 @@ package com.example.myaccountbook.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.RadioButton
 import androidx.lifecycle.ViewModelProviders
 import com.example.myaccountbook.R
@@ -27,7 +28,14 @@ class AddItemActivity : AppCompatActivity() {
             && intent.hasExtra(AccountString().EXTRA_ACCOUNT_MONEY)
             && intent.hasExtra(AccountString().EXTRA_ACCOUNT_DATE)
         ) {
-            add_item_input_money.setText(intent.getIntExtra(AccountString().EXTRA_ACCOUNT_MONEY,0).toString())
+            add_item_add_button.text = "수정"
+            add_item_remove_button.visibility = View.VISIBLE
+            add_item_input_money.setText(
+                intent.getIntExtra(
+                    AccountString().EXTRA_ACCOUNT_MONEY,
+                    0
+                ).toString()
+            )
             add_item_input_money.setSelection(add_item_input_money.text.length)
             if (intent.getStringExtra(AccountString().EXTRA_ACCOUNT_CATEGORY) == AccountString().INCOME) {
                 add_item_radio_button_income.isChecked = true
@@ -38,14 +46,25 @@ class AddItemActivity : AppCompatActivity() {
         }
 
         add_item_add_button.setOnClickListener {
-            val categoryId = add_item_radio_group.checkedRadioButtonId
-            val category = findViewById<RadioButton>(categoryId).text.toString()
-            val money = add_item_input_money.text.toString().trim().toInt()
-            val date = intent.getStringExtra(AccountString().EXTRA_ACCOUNT_DATE)?:""
-
-            val account = AccountEntity(id, category, money, date)
+            val account = makeAccount()
             accountViewModel.insertAccount(account)
             finish()
         }
+
+        add_item_remove_button.setOnClickListener {
+            val account = makeAccount()
+            accountViewModel.deleteAccount(account)
+            finish()
+        }
+
+    }
+
+    private fun makeAccount(): AccountEntity {
+        val categoryId = add_item_radio_group.checkedRadioButtonId
+        val category = findViewById<RadioButton>(categoryId).text.toString()
+        val money = add_item_input_money.text.toString().trim().toInt()
+        val date = intent.getStringExtra(AccountString().EXTRA_ACCOUNT_DATE) ?: ""
+
+        return AccountEntity(id, category, money, date)
     }
 }
